8c6295cedf64dd6410d6528434f453687d36083a
KateZabolotskih <zabolotskihkate@gmail.com>
2020-10-11 20:49:31 +0300

    changed CMake files for the dll build

--- test/src/test.cpp
+++ test/src/test.cpp
@@ -1,4 +1,15 @@
 //
 // Created by Екатерина Заболотских on 10.10.2020.
 //
+#include "../include/ILogger.h"
+#include "../include/IVector.h"
 
+
+
+int main() {
+    int a;
+    double data[] = {1, 2, 3};
+    ILogger * logger = ILogger::createLogger(&a);
+    IVector * v1 = IVector::createVector(3, data, nullptr);
+    return 0;
+}
